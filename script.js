5a505e072939e40b2dd0eb25631963bf4f873f66
dimasiks123465789 <82897180+dimasiks123465789@users.noreply.github.com>
2021-04-20 19:32:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+<script data-ad-client="ca-pub-2047124653106415" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
